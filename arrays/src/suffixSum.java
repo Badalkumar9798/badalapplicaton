08579d33699a6e65422a8d505c6b1d425cbf27cf
Badal kumar <badalkumar88935@gmail.com>
2026-09-11 00:40:06 +0530

    suffix Sum Array

---
+++ arrays/src/suffixSum.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/** Find suffix sum array  **/
+
+public class suffixSum {
+    static int[] SuffixSum(int[] arr){
+
+        int n = arr.length;
+        int[] suffix = new int[n];
+
+        suffix[n-1] = arr[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            suffix[i] = arr[i] + suffix[i+1];
+        }
+        return suffix;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + "elements :- ");
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] suffix = SuffixSum(arr);
+        System.out.println("Suffix sum array :- ");
+
+        for(int i =0;i<n;i++){
+            System.out.print(suffix[i] + " ");
+        }
+    }
+}
